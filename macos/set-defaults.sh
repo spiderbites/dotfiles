6c7d387734dfadf05c64a3c6d754bbb409205c0a
Josh Zucker <josh.zucker@shopify.com>
2021-12-14 22:34:25 -0500

    fix some macos set defaults

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -8,16 +8,13 @@
 # Run ./set-defaults.sh and you'll be good to go.
 
 # Disable press-and-hold for keys in favor of key repeat.
-defaults write -g ApplePressAndHoldEnabled -bool false
-
-# Use AirDrop over every interface. srsly this should be a default.
-defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+defaults write -g ApplePressAndHoldEnabled -boolean false
 
 # Always open everything in Finder's list view. This is important.
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
 # Show the ~/Library folder.
-chflags nohidden ~/Library
+xattr -d com.apple.FinderInfo ~/Library
 
 # Set a really fast key repeat.
 defaults write NSGlobalDomain KeyRepeat -int 1

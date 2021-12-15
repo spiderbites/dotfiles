b4394efaa8eeb8db6499e84247d9984ae9e7dac4
Josh Zucker <josh.zucker@shopify.com>
2021-12-14 22:34:10 -0500

    fix show/hide hidden files alises

--- macos/aliases.zsh
+++ macos/aliases.zsh
@@ -1,3 +1,3 @@
 # show or hide hidden files in finder
-alias showhiddenfiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
-alias hidehiddenfiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+alias showhiddenfiles='defaults write com.apple.finder AppleShowAllFiles -boolean true; killall Finder'
+alias hidehiddenfiles='defaults write com.apple.finder AppleShowAllFiles -boolean false; killall Finder'

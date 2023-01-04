cbef38090030e45cef3015ce05533d175b7aeb4a
Josh Zucker <josh.zucker@shopify.com>
2023-01-04 12:40:27 -0500

    Add --force-if-includes to git pf alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -11,6 +11,7 @@ alias gca='git commit -a'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
+alias gbs='git branch --sort=-committerdate' # git branch sorted
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
@@ -40,7 +41,7 @@ function gr {
   git rebase -i HEAD~"$1"
 }
 
-function gitzip() { 
+function gitzip() {
 	git archive -o $@.zip HEAD
 }
 

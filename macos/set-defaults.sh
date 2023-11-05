897f8967a2604a225e9080ea269da121a733a40c
Josh Zucker <josh.zucker@shopify.com>
2023-11-04 22:33:39 -0700

    dock hiding and smol

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -26,3 +26,7 @@ defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
 # Show macOS app switcher across all monitors
 defaults write com.apple.Dock appswitcher-all-displays -bool true
+
+# Smaller dock icons
+defaults write com.apple.dock "tilesize" -int "36"
+defaults write com.apple.dock "autohide" -bool "true"

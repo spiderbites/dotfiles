4c72365fabd45721f8b4fcacbbba9106663074cc
Josh Zucker <zuckerje@gmail.com>
2025-11-15 16:34:56 -0500

    macos defaults

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -1,29 +1,21 @@
-# come back and check all these
+# Sets reasonable macOS defaults.
+# Run ./set-defaults.sh
 
-# # Sets reasonable macOS defaults.
-# #
-# # Or, in other words, set shit how I like in macOS.
-# #
-# # The original idea (and a couple settings) were grabbed from:
-# #   https://github.com/mathiasbynens/dotfiles/blob/master/.macos
-# #
-# # Run ./set-defaults.sh and you'll be good to go.
+# Disable press-and-hold for keys in favor of key repeat.
+defaults write -g ApplePressAndHoldEnabled -boolean false
 
-# # Disable press-and-hold for keys in favor of key repeat.
-# defaults write -g ApplePressAndHoldEnabled -boolean false
+# Show the ~/Library folder.
+chflags nohidden ~/Library
 
-# # Always open everything in Finder's list view. This is important.
-# defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+# Set a really fast key repeat.
+defaults write NSGlobalDomain KeyRepeat -int 1
 
-# # Show the ~/Library folder.
-# xattr -d com.apple.FinderInfo ~/Library
+# Set the Finder prefs for showing a few different volumes on the Desktop.
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
-# # Set a really fast key repeat.
-# defaults write NSGlobalDomain KeyRepeat -int 1
+# Show macOS app switcher across all monitors
+defaults write com.apple.Dock appswitcher-all-displays -bool true
 
-# # Set the Finder prefs for showing a few different volumes on the Desktop.
-# defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-# defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
-
-# # Show macOS app switcher across all monitors
-# defaults write com.apple.Dock appswitcher-all-displays -bool true
+# Save screenshots to clipboard
+defaults write com.apple.screencapture target clipboard
